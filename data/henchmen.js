2898427b383d4fdabc337b76d72108b588b9e3fa
Kieran Fleenor <kieranfleenor@comcast.net>
2025-11-12 14:53:58 -0500

    added villain deck single editing

--- data/henchmen.js
+++ data/henchmen.js
@@ -1,7 +1,7 @@
 const cardArtFolder = "https://raw.githubusercontent.com/over-lords/overlords/4c0f2468199e5fcd6ee3a996f5803d11a9c9d981/Public/Images/Card%20Assets/Henchmen";
 
 export const henchmen = [
-    {
+  {
     id: "1",
     name: "Joker Gang",
     image: `${cardArtFolder}/Joker Gang.jpg`,
@@ -25,6 +25,120 @@ export const henchmen = [
         uses: `1`,
         shared: `no`,
         effect: `rescueBystander(1,random)`
+      }
+    ]
+  },
+  {
+    id: "2",
+    name: "Mobsters",
+    image: `${cardArtFolder}/Mobsters.jpg`,
+    type: "Henchman",
+    hp: "1",
+    damage: "1",
+    abilitiesText: [
+      {
+        text: `Reward: Draw a card.`
+      }
+    ],
+    abilitiesNamePrint: [
+      {
+        text: `Reward!`
+      }
+    ],
+    abilitiesEffects: [
+      {
+        type: `uponDefeat`,
+        condition: `none`,
+        uses: `1`,
+        shared: `no`,
+        effect: `draw(1)`
+      }
+    ]
+  },
+  {
+    id: "3",
+    name: "Parademons",
+    image: `${cardArtFolder}/Parademons.jpg`,
+    type: "Henchman",
+    hp: "2",
+    damage: "2",
+    abilitiesText: [
+      {
+        text: `Teleport <span class="line-gap"></span>
+               Reward: Your Hero's Travel Budget increases by 1 for this turn.`
+      }
+    ],
+    abilitiesNamePrint: [
+      {
+        text: `Boomtube`
+      },
+      {
+        text: `Reward!`
+      }
+    ],
+    abilitiesEffects: [
+      {
+        type: `quick`,
+        condition: `onEntry`,
+        uses: `1`,
+        shared: `no`,
+        effect: `teleport(random)`
+      },
+      {
+        type: `uponDefeat`,
+        condition: `none`,
+        uses: `1`,
+        shared: `no`,
+        effect: `draw(1)`
+      }
+    ]
+  },
+  {
+    id: "4",
+    name: "Brainiac Drones",
+    image: `${cardArtFolder}/Braniac Drones.jpg`,
+    type: "Henchman",
+    hp: "3",
+    damage: "3",
+    abilitiesText: [
+      {
+        text: `Teleport <span class="line-gap"></span>
+               If your Hero is KO'd by a Brainiac Drone: Next turn do not draw from the Villain Deck, your Hero enters as a Villain. <span class="line-gap"></span>
+               Reward: You may draw from the Enemies and Allies Pile.`
+      }
+    ],
+    abilitiesNamePrint: [
+      {
+        text: `Dropship`
+      },
+      {
+        text: `Become my thrall`
+      },
+      {
+        text: `Reward!`
+      }
+    ],
+    abilitiesEffects: [
+      {
+        type: `quick`,
+        condition: `onEntry`,
+        uses: `1`,
+        shared: `no`,
+        effect: `teleport(random)`
+      },
+      {
+        type: `quick`,
+        condition: `KOHero`,
+        uses: `999`,
+        shared: `no`,
+        effect: `returnHeroAsVillain`
+      },
+      {
+        type: `uponDefeat`,
+        condition: `none`,
+        uses: `1`,
+        shared: `no`,
+        effect: `draw(1)`
       }
     ]
   },
