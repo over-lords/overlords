7778533ccae787773dd64e979d2fc62744a14dae
Kieran Fleenor <kieranfleenor@comcast.net>
2025-11-12 14:57:37 -0500

    all bystanders in

--- data/bystanders.js
+++ data/bystanders.js
@@ -32,4 +32,79 @@ export const bystanders = [
             }
         ]
     },
+    {
+        id: "3",
+        name: "Captured Argus Agent",
+        image: `${cardArtFolder}/Argus Agent.jpg`,
+        type: "Bystander",
+        abilitiesText: [
+            {
+                text: `You may draw from the Enemies and Allies Pile.`
+            }
+        ],
+        abilitiesNamePrint: [
+            {
+                text: `You may draw from the Enemies and Allies Pile.`
+            }
+        ],
+        abilitiesEffects: [
+            {
+                type: `standard`,
+                condition: `none`,
+                uses: `1`,
+                shared: `no`,
+                effect: `drawEaA(1)`
+            }
+        ]
+    },
+    {
+        id: "4",
+        name: "Medic",
+        image: `${cardArtFolder}/Medic.jpg`,
+        type: "Bystander",
+        abilitiesText: [
+            {
+                text: `Regain 2 HP.`
+            }
+        ],
+        abilitiesNamePrint: [
+            {
+                text: `Regain 2 HP.`
+            }
+        ],
+        abilitiesEffects: [
+            {
+                type: `standard`,
+                condition: `none`,
+                uses: `1`,
+                shared: `no`,
+                effect: `gainLife(2)`
+            }
+        ]
+    },
+    {
+        id: "5",
+        name: "Captured Sidekick",
+        image: `${cardArtFolder}/Sidekick.jpg`,
+        type: "Bystander",
+        abilitiesText: [
+            {
+                text: `Gain a Sidekick.`
+            }
+        ],
+        abilitiesNamePrint: [
+            {
+                text: `Gain a Sidekick.`
+            }
+        ],
+        abilitiesEffects: [
+            {
+                type: `standard`,
+                condition: `none`,
+                uses: `1`,
+                shared: `no`,
+                effect: `gainSidekick(1)`
+            }
+        ]
+    },
 ]
