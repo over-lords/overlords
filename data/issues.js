96b9ff2582d52512ca95e56cf42f7d58df15c334
Kieran Fleenor <kieranfleenor@comcast.net>
2025-11-18 08:23:54 -0500

    infobox style update

--- data/issues.js
+++ data/issues.js
@@ -1,6 +1,6 @@
 const issueData = [
     {
         title: "Welcome!",
-        text: "."
+        text: "Overlords is happy to have YOU as a player! This is a game in pre-alpha, things are going to be messy. Please email the head developer at kieranfleenor@gmail.com if you have any issues you'd like to make sure we know about!"
     }
 ];
--- data/news.js
+++ data/news.js
@@ -1,6 +1,10 @@
 const newsData = [
     {
+        date: "November 18th, 2025",
+        text: "Finally started working on game code. Made it through accounts, data entry, rules, setup, and all other preparatory steps."
+    },
+    {
         date: "September 23rd, 2025",
-        text: "."
+        text: "Started development."
     }
 ];
