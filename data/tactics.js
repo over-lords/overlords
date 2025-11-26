0f691533acecab71466a41f65ce921a937bd39b1
Kieran Fleenor <kieranfleenor@comcast.net>
2025-11-26 12:00:18 -0500

    tactics added

--- data/tactics.js
+++ data/tactics.js
@@ -122,7 +122,7 @@ export const tactics = [
     abilitiesText: [
       {
         text: `Required: 2+ Overlords, 2 Tactics, Enemies-Only (30 minimum). <span class="line-gap"></span>
-              Might of the Overlord: Draw twice from the Enemies and Allies Pile, and once from the Villain Deck.`
+              Might of the Overlord: Draw 2 from the E&A, and once from the Villain Deck.`
       }
     ],
     abilitiesNamePrint: [
@@ -150,7 +150,7 @@ export const tactics = [
         condition: `might`,
         uses: `999`,
         shared: `no`,
-        effect: `enemiesDraw(2),villainDraw(1)`
+        effect: [`enemiesDraw(2)`,`villainDraw(1)`]
       }
     ],
     evilWinsNamePrint: [
@@ -281,6 +281,536 @@ export const tactics = [
         uses: `0`,
         shared: `no`,
         effect: `none`
+      }
+    ]
+  },
+  {
+    id: "5405",
+    name: "Computer Virus",
+    image: `${cardArtFolder}/Tactic.jpg`,
+    type: "Tactic",
+    doNotShow: "true",
+    limitEaA: "no",
+    multiTacticReq: "no",
+    multiOverlordReq: "no",
+    abilitiesText: [
+      {
+        text: `No Heroes can Scan. <span class="line-gap"></span>
+                Might of the Overlord: KO the top card of every Heroes' deck.`
+      }
+    ],
+    abilitiesNamePrint: [
+      {
+        text: `na`
+      }
+    ],
+    abilitiesEffects: [
+      {
+        type: `none`,
+        condition: `none`,
+        uses: `0`,
+        shared: `no`,
+        effect: `none`
+      }
+    ],
+    mightNamePrint: [
+      {
+        text: `Brzzt! Deletion Successful!`
+      }
+    ],
+    mightEffects: [
+      {
+        type: `might`,
+        condition: `might`,
+        uses: `999`,
+        shared: `no`,
+        effect: `koTopHeroCard(all,1)`
+      }
+    ],
+    evilWinsNamePrint: [
+      {
+        text: `Evil Wins!`
+      }
+    ],
+    evilWinsEffects: [
+      {
+        type: `evilWins`,
+        condition: `none`,
+        uses: `1`,
+        shared: `no`,
+        effect: `evilWins`
+      }
+    ]
+  },
+  {
+    id: "5406",
+    name: "Coming of Apokalips",
+    image: `${cardArtFolder}/Tactic.jpg`,
+    type: "Tactic",
+    doNotShow: "true",
+    limitEaA: "no",
+    multiTacticReq: "no",
+    multiOverlordReq: "no",
+    abilitiesText: [
+      {
+        text: `Might of the Overlord: Draw from the E&A. <span class="line-gap"></span> Also, the current turn Hero must CHOOSE: KO 1 Bystander. <span class="line-gap"></span> OR <span class="line-gap"></span> Destroy the Rightmost City. <span class="line-gap"></span><span class="line-gap"></span>
+              Bonus Evil Wins Condition: Evil Wins when 10 Bystanders have been KO'd.`
+      }
+    ],
+    abilitiesNamePrint: [
+      {
+        text: `na`
+      }
+    ],
+    abilitiesEffects: [
+      {
+        type: `none`,
+        condition: `none`,
+        uses: `0`,
+        shared: `no`,
+        effect: `none`
+      }
+    ],
+    mightNamePrint: [
+      {
+        text: `Additional Chaos!`
+      },
+      {
+        text: `Choose!`
+      }
+    ],
+    mightEffects: [
+      {
+        type: `might`,
+        condition: `might`,
+        uses: `999`,
+        shared: `no`,
+        effect: [`enemiesDraw(1)`]
+      },
+      {
+        type: `choose()`,
+        condition: `might`,
+        uses: `999`,
+        shared: `no`,
+        effect: [`koBystander(1)`,`destroyNextCity()`]
+      }
+    ],
+    evilWinsNamePrint: [
+      {
+        text: `Evil Wins!`
+      }
+    ],
+    evilWinsEffects: [
+      {
+        type: `evilWins`,
+        condition: `bystandersKOD(10)`,
+        uses: `1`,
+        shared: `no`,
+        effect: `evilWins`
+      }
+    ]
+  },
+  {
+    id: "5407",
+    name: "Overwhelming Might",
+    image: `${cardArtFolder}/Tactic.jpg`,
+    type: "Tactic",
+    doNotShow: "true",
+    limitEaA: ["no"],
+    multiTacticReq: "no",
+    multiOverlordReq: "no",
+    abilitiesText: [
+      {
+        text: `Might of the Overlord: Draw 5 times from the Villain Deck (ignore additional Mights of the Overlord drawn by this effect).`
+      }
+    ],
+    abilitiesNamePrint: [
+      {
+        text: `none`
+      }
+    ],
+    abilitiesEffects: [
+      {
+        type: `none`,
+        condition: `none`,
+        uses: `0`,
+        shared: `no`,
+        effect: `none`
+      }
+    ],
+    mightNamePrint: [
+      {
+        text: `Why did you say that?!`
+      }
+    ],
+    mightEffects: [
+      {
+        type: `might`,
+        condition: `might`,
+        uses: `999`,
+        shared: `no`,
+        effect: `drawVillainIgnoreMights(5)`
+      }
+    ],
+    evilWinsNamePrint: [
+      {
+        text: `none`
+      }
+    ],
+    evilWinsEffects: [
+      {
+        type: `none`,
+        condition: `none`,
+        uses: `0`,
+        shared: `no`,
+        effect: `none`
+      }
+    ]
+  },
+  {
+    id: "5408",
+    name: "Welcome to the Madhouse",
+    image: `${cardArtFolder}/Tactic.jpg`,
+    type: "Tactic",
+    doNotShow: "true",
+    limitEaA: "no",
+    multiTacticReq: "no",
+    multiOverlordReq: "no",
+    abilitiesText: [
+      {
+        text: `KO'd Heroes enter the board as Villains. <span class="line-gap"></span>
+                Might of the Overlord: Engaged Heroes take their foe's Damage.`
+      }
+    ],
+    abilitiesNamePrint: [
+      {
+        text: `Welcome Home!`
+      }
+    ],
+    abilitiesEffects: [
+      {
+        type: `passive`,
+        condition: `heroKod()`,
+        uses: `0`,
+        shared: `no`,
+        effect: `returnHeroAsVillain()`
+      }
+    ],
+    mightNamePrint: [
+      {
+        text: `On My Command!`
+      }
+    ],
+    mightEffects: [
+      {
+        type: `might`,
+        condition: `might`,
+        uses: `999`,
+        shared: `no`,
+        effect: `heroTakeFoeDamage(all)`
+      }
+    ],
+    evilWinsNamePrint: [
+      {
+        text: `Evil Wins!`
+      }
+    ],
+    evilWinsEffects: [
+      {
+        type: `evilWins`,
+        condition: `none`,
+        uses: `1`,
+        shared: `no`,
+        effect: `evilWins`
+      }
+    ]
+  },
+  {
+    id: "5409",
+    name: "Thanagar Strikes",
+    image: `${cardArtFolder}/Tactic.jpg`,
+    type: "Tactic",
+    doNotShow: "true",
+    limitEaA: "no",
+    multiTacticReq: "no",
+    multiOverlordReq: "no",
+    abilitiesText: [
+      {
+        text: `All Villains gain Glide. <span class="line-gap"></span>
+                Might of the Overlord: Draw 3 times from the Villain Deck (ignore additional Mights of the Overlord drawn by this effect).`
+      }
+    ],
+    abilitiesNamePrint: [
+      {
+        text: `Have Some Wings!`
+      }
+    ],
+    abilitiesEffects: [
+      {
+        type: `passive`,
+        condition: `villainDrawn()`,
+        uses: `0`,
+        shared: `no`,
+        effect: `villainGainGlide()`
+      }
+    ],
+    mightNamePrint: [
+      {
+        text: `Drop Ship Approaching!`
+      }
+    ],
+    mightEffects: [
+      {
+        type: `might`,
+        condition: `might`,
+        uses: `999`,
+        shared: `no`,
+        effect: `drawVillainIgnoreMights(3)`
+      }
+    ],
+    evilWinsNamePrint: [
+      {
+        text: `Evil Wins!`
+      }
+    ],
+    evilWinsEffects: [
+      {
+        type: `evilWins`,
+        condition: `none`,
+        uses: `1`,
+        shared: `no`,
+        effect: `evilWins`
+      }
+    ]
+  },
+  {
+    id: "5410",
+    name: "In Blackest Night",
+    image: `${cardArtFolder}/Tactic.jpg`,
+    type: "Tactic",
+    doNotShow: "true",
+    limitEaA: "no",
+    multiTacticReq: "no",
+    multiOverlordReq: "no",
+    abilitiesText: [
+      {
+        text: `KO'd Heroes enter the board as Villains. <span class="line-gap"></span>
+                Might of the Overlord: Revive the first KO'd Henchman or Villain.`
+      }
+    ],
+    abilitiesNamePrint: [
+      {
+        text: `Rise!`
+      }
+    ],
+    abilitiesEffects: [
+      {
+        type: `passive`,
+        condition: `heroKod()`,
+        uses: `0`,
+        shared: `no`,
+        effect: `returnHeroAsVillain()`
+      }
+    ],
+    mightNamePrint: [
+      {
+        text: `Emotion Detected!`
+      }
+    ],
+    mightEffects: [
+      {
+        type: `might`,
+        condition: `might`,
+        uses: `999`,
+        shared: `no`,
+        effect: `reviveKodFoe(first)`
+      }
+    ],
+    evilWinsNamePrint: [
+      {
+        text: `Evil Wins!`
+      }
+    ],
+    evilWinsEffects: [
+      {
+        type: `evilWins`,
+        condition: `none`,
+        uses: `1`,
+        shared: `no`,
+        effect: `evilWins`
+      }
+    ]
+  },
+  {
+    id: "5411",
+    name: "Systematic Destruction",
+    image: `${cardArtFolder}/Tactic.jpg`,
+    type: "Tactic",
+    doNotShow: "true",
+    limitEaA: ["no"],
+    multiTacticReq: "no",
+    multiOverlordReq: "no",
+    abilitiesText: [
+      {
+        text: `Might of the Overlord: All Heroes must KO a card from either their Hand or discard pile.`
+      }
+    ],
+    abilitiesNamePrint: [
+      {
+        text: `none`
+      }
+    ],
+    abilitiesEffects: [
+      {
+        type: `none`,
+        condition: `none`,
+        uses: `0`,
+        shared: `no`,
+        effect: `none`
+      }
+    ],
+    mightNamePrint: [
+      {
+        text: `Zap!`
+      }
+    ],
+    mightEffects: [
+      {
+        type: `might`,
+        condition: `might`,
+        uses: `999`,
+        shared: `no`,
+        effect: `koCardFromHandORDiscard(1)`
+      }
+    ],
+    evilWinsNamePrint: [
+      {
+        text: `none`
+      }
+    ],
+    evilWinsEffects: [
+      {
+        type: `none`,
+        condition: `none`,
+        uses: `0`,
+        shared: `no`,
+        effect: `none`
+      }
+    ]
+  },
+  {
+    id: "5412",
+    name: "Forever Evil",
+    image: `${cardArtFolder}/Tactic.jpg`,
+    type: "Tactic",
+    doNotShow: "true",
+    limitEaA: ["no"],
+    multiTacticReq: "yes",
+    multiOverlordReq: "no",
+    abilitiesText: [
+      {
+        text: `When you KO a Henchman: OPTIONAL: Ignore their Reward and deal their Damage to another Henchman, Villain, or the Overlord. <span class="line-gap"></span>
+              Might of the Overlord: All Heroes take Damage equal to the number of rescued Bystanders.`
+      }
+    ],
+    abilitiesNamePrint: [
+      {
+        text: `Throw the goons!`
+      }
+    ],
+    abilitiesEffects: [
+      {
+        type: `quick`,
+        condition: `kodHenchman()`,
+        uses: `0`,
+        shared: `no`,
+        effect: `ignoreRewardDamageOverlord(henchmanDamage())`
+      }
+    ],
+    mightNamePrint: [
+      {
+        text: `Disguised Bystanders!`
+      }
+    ],
+    mightEffects: [
+      {
+        type: `might`,
+        condition: `might`,
+        uses: `999`,
+        shared: `no`,
+        effect: [`damageHero(all,rescuedBystandersCount())`]
+      }
+    ],
+    evilWinsNamePrint: [
+      {
+        text: `none`
+      }
+    ],
+    evilWinsEffects: [
+      {
+        type: `none`,
+        condition: `none`,
+        uses: `0`,
+        shared: `no`,
+        effect: `none`
+      }
+    ]
+  },
+  {
+    id: "5413",
+    name: "Year of the Villain",
+    image: `${cardArtFolder}/Tactic.jpg`,
+    type: "Tactic",
+    doNotShow: "true",
+    limitEaA: ["no"],
+    multiTacticReq: "yes",
+    multiOverlordReq: "no",
+    abilitiesText: [
+      {
+        text: `OPTIONAL: When you rescue a Bystander, you can instead choose to KO them (doing so means you do not gain their benefits). <span class="line-gap"></span>
+              Bonus Heroes Win Condition: You win when 10 Bystanders have been KO'd.`
+      }
+    ],
+    abilitiesNamePrint: [
+      {
+        text: `I Don't Have Time for This!`
+      }
+    ],
+    abilitiesEffects: [
+      {
+        type: `quick`,
+        condition: `rescueBystander()`,
+        uses: `0`,
+        shared: `no`,
+        effect: `koRescuedBystander()`
+      }
+    ],
+    mightNamePrint: [
+      {
+        text: `none`
+      }
+    ],
+    mightEffects: [
+      {
+        type: `none`,
+        condition: `none`,
+        uses: `999`,
+        shared: `no`,
+        effect: [`none`]
+      }
+    ],
+    heroWinsNamePrint: [
+      {
+        text: `We Did It Gang!`
+      }
+    ],
+    heroWinsEffects: [
+      {
+        type: `heroWins`,
+        condition: `bystandersKOD(10)`,
+        uses: `1`,
+        shared: `no`,
+        effect: `heroWins`
       }
     ]
   },
