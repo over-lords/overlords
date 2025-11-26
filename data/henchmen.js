6479b71395ce0361510588f07d40988ba76d652b
Kieran Fleenor <kieranfleenor@comcast.net>
2025-11-26 10:48:40 -0500

    all henchmen added into js

--- data/henchmen.js
+++ data/henchmen.js
@@ -41,7 +41,7 @@ export const henchmen = [
     damage: "1",
     abilitiesText: [
       {
-        text: `Reward: Draw a card.`
+        text: `Reward: Draw 1.`
       }
     ],
     abilitiesNamePrint: [
@@ -141,7 +141,7 @@ export const henchmen = [
       },
       {
         type: `uponDefeat`,
-        condition: `none`,
+        condition: `optional()`,
         uses: `1`,
         shared: `no`,
         effect: `enaDraw(1,0)`
@@ -169,7 +169,7 @@ export const henchmen = [
     abilitiesEffects: [
       {
         type: `uponDefeat`,
-        condition: `none`,
+        condition: `optional()`,
         uses: `1`,
         shared: `no`,
         effect: `enaDraw(1,0)`
@@ -208,7 +208,7 @@ export const henchmen = [
       },
       {
         type: `uponDefeat`,
-        condition: `none`,
+        condition: `optional()`,
         uses: `1`,
         shared: `no`,
         effect: `returnHenchOrVillain(1)`
@@ -251,6 +251,392 @@ export const henchmen = [
         uses: `1`,
         shared: `no`,
         effect: `regainLife(1)`
+      }
+    ]
+  },
+  {
+    id: "4858",
+    name: "Gorilla Soldiers",
+    image: `${cardArtFolder}/gorillaSoldiers.jpg`,
+    type: "Henchman",
+    doNotShow: "true",
+    hp: "1",
+    damage: "1",
+    abilitiesText: [
+      {
+        text: `Reward: Deal 1 Damage to the Overlord.`
+      }
+    ],
+    abilitiesNamePrint: [
+      {
+        text: `Reward!`
+      }
+    ],
+    abilitiesEffects: [
+      {
+        type: `uponDefeat`,
+        condition: `none`,
+        uses: `1`,
+        shared: `no`,
+        effect: `damageOverlord(1)`
+      }
+    ]
+  },
+  {
+    id: "4859",
+    name: "Black Lanterns",
+    image: `${cardArtFolder}/blackLanterns.jpg`,
+    type: "Henchman",
+    doNotShow: "true",
+    hp: "3",
+    damage: "3",
+    abilitiesText: [
+      {
+        text: `Teleport <span class="line-gap"></span>
+               Reward: Permanently KO a Henchman or Villain in the KO Pile.`
+      }
+    ],
+    abilitiesNamePrint: [
+      {
+        text: `Death From Above!`
+      },
+      {
+        text: `Reward!`
+      }
+    ],
+    abilitiesEffects: [
+      {
+        type: `quick`,
+        condition: `onEntry()`,
+        uses: `0`,
+        shared: `no`,
+        effect: `teleport()`
+      },
+      {
+        type: `uponDefeat`,
+        condition: `none`,
+        uses: `1`,
+        shared: `no`,
+        effect: `koFromKO(1)`
+      }
+    ]
+  },
+  {
+    id: "4860",
+    name: "League of Assassins Ninjas",
+    image: `${cardArtFolder}/leagueOfAssassinsNinjas.jpg`,
+    type: "Henchman",
+    doNotShow: "true",
+    hp: "1",
+    damage: "1",
+    abilitiesText: [
+      {
+        text: `Reward: Your Hero regains 1 HP.`
+      }
+    ],
+    abilitiesNamePrint: [
+      {
+        text: `Reward!`
+      }
+    ],
+    abilitiesEffects: [
+      {
+        type: `uponDefeat`,
+        condition: `none`,
+        uses: `1`,
+        shared: `no`,
+        effect: `regainLife(1)`
+      }
+    ]
+  },
+  {
+    id: "4861",
+    name: "Creatures of Legend",
+    image: `${cardArtFolder}/creaturesOfLegend3.jpg`,
+    type: "Henchman",
+    doNotShow: "true",
+    hp: "2",
+    damage: "2",
+    abilitiesText: [
+      {
+        text: `Reward: Deal 2 Damage to the Overlord.`
+      }
+    ],
+    abilitiesNamePrint: [
+      {
+        text: `Reward!`
+      }
+    ],
+    abilitiesEffects: [
+      {
+        type: `uponDefeat`,
+        condition: `none`,
+        uses: `1`,
+        shared: `no`,
+        effect: `damageOverlord(2)`
+      }
+    ]
+  },
+  {
+    id: "4862",
+    name: "Atlantean Deserters",
+    image: `${cardArtFolder}/atlanteanDeserters.jpg`,
+    type: "Henchman",
+    doNotShow: "true",
+    hp: "1",
+    damage: "1",
+    abilitiesText: [
+      {
+        text: `Reward: Draw 2.`
+      }
+    ],
+    abilitiesNamePrint: [
+      {
+        text: `Reward!`
+      }
+    ],
+    abilitiesEffects: [
+      {
+        type: `uponDefeat`,
+        condition: `none`,
+        uses: `1`,
+        shared: `no`,
+        effect: `draw(2)`
+      }
+    ]
+  },
+  {
+    id: "4863",
+    name: "Sinestro Corps",
+    image: `${cardArtFolder}/sinestroCorps.jpg`,
+    type: "Henchman",
+    doNotShow: "true",
+    hp: "2",
+    damage: "2",
+    abilitiesText: [
+      {
+        text: `Reward: OPTIONAL: Draw from the E&A.`
+      }
+    ],
+    abilitiesNamePrint: [
+      {
+        text: `Reward!`
+      }
+    ],
+    abilitiesEffects: [
+      {
+        type: `uponDefeat`,
+        condition: `optional()`,
+        uses: `1`,
+        shared: `no`,
+        effect: `enaDraw(1,0)`
+      }
+    ]
+  },
+  {
+    id: "4864",
+    name: "Omac Drones",
+    image: `${cardArtFolder}/omacDrones.jpg`,
+    type: "Henchman",
+    doNotShow: "true",
+    hp: "3",
+    damage: "3",
+    abilitiesText: [
+      {
+        text: `If your Hero is KO'd by an Omac Drone: Next turn do not draw from the Villain Deck, your Hero enters as a Villain. <span class="line-gap"></span>
+               Reward: Draw 2, and your Hero's Travel Budget increases by 1 for this turn.`
+      }
+    ],
+    abilitiesNamePrint: [
+      {
+        text: `Eye Am Reborn!`
+      },
+      {
+        text: `Reward!`
+      }
+    ],
+    abilitiesEffects: [
+      {
+        type: `quick`,
+        condition: `KOHero`,
+        uses: `999`,
+        shared: `no`,
+        effect: `returnHeroAsVillain`
+      },
+      {
+        type: `uponDefeat`,
+        condition: `none`,
+        uses: `1`,
+        shared: `no`,
+        effect: `drawPlusTravel(2,1)`
+      }
+    ]
+  },
+  {
+    id: "4865",
+    name: "Arkham Guards",
+    image: `${cardArtFolder}/arkhamGuards.jpg`,
+    type: "Henchman",
+    doNotShow: "true",
+    hp: "1",
+    damage: "1",
+    abilitiesText: [
+      {
+        text: `Reward: Draw 1, it is used against the Overlord.`
+      }
+    ],
+    abilitiesNamePrint: [
+      {
+        text: `Reward!`
+      }
+    ],
+    abilitiesEffects: [
+      {
+        type: `uponDefeat`,
+        condition: `none`,
+        uses: `1`,
+        shared: `no`,
+        effect: `drawUseOverlord(1)`
+      }
+    ]
+  },
+  {
+    id: "4866",
+    name: "Argus Security",
+    image: `${cardArtFolder}/argusSecurity.jpg`,
+    type: "Henchman",
+    doNotShow: "true",
+    hp: "2",
+    damage: "2",
+    abilitiesText: [
+      {
+        text: `Reward: Scan 1 from the Villain Deck, it is not drawn next turn.`
+      }
+    ],
+    abilitiesNamePrint: [
+      {
+        text: `Reward!`
+      }
+    ],
+    abilitiesEffects: [
+      {
+        type: `uponDefeat`,
+        condition: `none`,
+        uses: `1`,
+        shared: `no`,
+        effect: `revealAndStopVillainDraw(1)`
+      }
+    ]
+  },
+  {
+    id: "4867",
+    name: "Enhanced Enforcer",
+    image: `${cardArtFolder}/enhancedEnforcer.jpg`,
+    type: "Henchman",
+    doNotShow: "true",
+    hp: "3",
+    damage: "3",
+    abilitiesText: [
+      {
+        text: `Reward: Scan 3 from the Villain Deck, KO all revealed Might of the Overlords.`
+      }
+    ],
+    abilitiesNamePrint: [
+      {
+        text: `Reward!`
+      }
+    ],
+    abilitiesEffects: [
+      {
+        type: `uponDefeat`,
+        condition: `none`,
+        uses: `1`,
+        shared: `no`,
+        effect: `scanKoMights(3)`
+      }
+    ]
+  },
+  {
+    id: "4868",
+    name: "Police Officers",
+    image: `${cardArtFolder}/policeOfficers.jpg`,
+    type: "Henchman",
+    doNotShow: "true",
+    hp: "1",
+    damage: "1",
+    abilitiesText: [
+      {
+        text: `Reward: Draw 1, and your Hero's Travel Budget increases by 1 for this turn.`
+      }
+    ],
+    abilitiesNamePrint: [
+      {
+        text: `Reward!`
+      }
+    ],
+    abilitiesEffects: [
+      {
+        type: `uponDefeat`,
+        condition: `none`,
+        uses: `1`,
+        shared: `no`,
+        effect: `drawPlusTravel(1,1)`
+      }
+    ]
+  },
+  {
+    id: "4869",
+    name: "Watchtower Security",
+    image: `${cardArtFolder}/watchtowerSecurity.jpg`,
+    type: "Henchman",
+    doNotShow: "true",
+    hp: "2",
+    damage: "2",
+    abilitiesText: [
+      {
+        text: `Reward: Rescue 2 Bystanders.`
+      }
+    ],
+    abilitiesNamePrint: [
+      {
+        text: `Reward!`
+      }
+    ],
+    abilitiesEffects: [
+      {
+        type: `uponDefeat`,
+        condition: `none`,
+        uses: `1`,
+        shared: `no`,
+        effect: `rescueBystander(2,random)`
+      }
+    ]
+  },
+  {
+    id: "4870",
+    name: "Minor Hero",
+    image: `${cardArtFolder}/minorHero.jpg`,
+    type: "Henchman",
+    doNotShow: "true",
+    hp: "3",
+    damage: "3",
+    abilitiesText: [
+      {
+        text: `Reward: OPTIONAL: KO the top card of your Hero's deck and deal 5 Damage to the Overlord.`
+      }
+    ],
+    abilitiesNamePrint: [
+      {
+        text: `Reward!`
+      }
+    ],
+    abilitiesEffects: [
+      {
+        type: `uponDefeat`,
+        condition: `optional()`,
+        uses: `1`,
+        shared: `no`,
+        effect: `koTopCardDamageOverlord(1,5)`
       }
     ]
   },
