dfa4a1234a5f74a28c37a663133565019cc37ef1
Kieran Fleenor <kieranfleenor@comcast.net>
2025-12-10 14:09:42 -0500

    text update

--- data/news.js
+++ data/news.js
@@ -1,5 +1,9 @@
 const newsData = [
     {
+        date: "December 10th, 2025",
+        text: "Basic game stuff is in for singleplayer. Good progress all around."
+    },
+    {
         date: "November 18th, 2025",
         text: "Finally started working on game code. Made it through accounts, data entry, rules, setup, and all other preparatory steps."
     },
