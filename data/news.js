7210f61abfe7b1e62cbf36733847666e90af25b0
Kieran Fleenor <kieranfleenor@comcast.net>
2025-12-27 18:24:25 -0500

    Text update

--- data/news.js
+++ data/news.js
@@ -1,14 +1,58 @@
 const newsData = [
     {
+        date: "December 24th, 2025",
+        text: "Beginner game nearly ready."
+    },
+    {
+        date: "December 11th, 2025",
+        text: "Mostly sorted things out."
+    },
+    {
         date: "December 10th, 2025",
         text: "Basic game stuff is in for singleplayer. Good progress all around."
     },
     {
+        date: "December 4th, 2025",
+        text: "Per card UUIDs, lots of other text updates and villain deck parsing."
+    },
+    {
+        date: "December 3rd, 2025",
+        text: "PWA v2!"
+    },
+    {
+        date: "December 2nd, 2025",
+        text: "Most cards are in, even if they are blank."
+    },
+    {
+        date: "November 24th, 2025",
+        text: "Many panels now function to parse data better."
+    },
+    {
         date: "November 18th, 2025",
-        text: "Finally started working on game code. Made it through accounts, data entry, rules, setup, and all other preparatory steps."
+        text: "Started working on game code."
+    },
+    {
+        date: "November 17th, 2025",
+        text: "Added users, login/logout, registering, databases, and a primitive service worker."
+    },
+    {
+        date: "November 12th, 2025",
+        text: "Custom singleplayer game with random buttons porting data to game.html now working."
+    },
+    {
+        date: "November 11th, 2025",
+        text: "Made all data storage files, added at least 1 entry to each."
+    },
+    {
+        date: "November 10th, 2025",
+        text: "Resumed work after a Hiatus. Got card rendering working."
     },
     {
         date: "September 23rd, 2025",
+        text: "Started work on the site (home page, roster, rules, etc)."
+    },
+    {
+        date: "September 15th, 2025",
         text: "Started development."
     }
 ];
